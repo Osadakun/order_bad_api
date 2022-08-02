03e9b06fd301668e9bf1bd96c96045a441022295
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-02 17:49:43 +0900

    UserモデルとPostモデルを1対多で関連付ける

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_080738) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_084353) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_080738) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
