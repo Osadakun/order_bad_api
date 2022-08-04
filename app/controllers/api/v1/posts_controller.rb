d6ddc3919f50fb51f74a41a3f74a459dfa34363a
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-04 23:13:23 +0900

    ならってpost_controllerの修正

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,46 +1,61 @@
 class Api::V1::PostsController < ApplicationController
-  before_action :authenticate_api_v1_user!, only: [:create, :update, :destroy] # 追加
-  def index
-    posts = Post.all.order(created_at: :desc)
-    render json: posts
-  end
-  def show
-    post = Post.find(params[:id])
-    render json: post
-  end
-  def create
-    post = Post.new(post_params)
-    if post.save
-      render json: post
-    else
-      render json: post.errors, status: 422
-    end
-  end
-  def update
-    post = Post.find(params[:id])
-    if current_api_v1_user.id == post.user_id
-      if post.update(post_params)
-      render json: post
-      else
-        render json: post.errors, status: 422
-      end
-    else
-      render json: {message: 'can not update data'}, status: 422
-    end
-  end
+	before_action :authenticate_api_v1_user!, only: [:create, :update, :destroy]
+	def index
+			posts = Post.all.order(created_at: :desc)
+			posts_array = posts.map do |post|
+					{
+							id: post.id,
+							content: post.content,
+							user: User.find_by(id: post.user_id)
+					}
+			end
+			render json: posts_array
+	end
+
+	def show
+			post = Post.find(params[:id])
+			post_list = {
+					id: post.id,
+					content: post.content,
+					user: post.user
+			}
+			render json: post_list
+	end
+
+	def create
+			post = Post.new(post_params)
+			if post.save
+					render json: post
+			else
+					render json: post.errors, status: 422
+			end
+	end
+
+	def update
+			post = Post.find(params[:id])
+			if current_api_v1_user.id == post.user_id
+					if post.update(post_params)
+							render json: post
+					else
+							render json: post.errors, status: 422
+					end
+			else
+					render json: {message: 'can not update data'}, status: 422
+			end
+	end
 
 	def destroy
-    post = Post.find(params[:id])
-    if current_api_v1_user.id == post.user_id
-      post.destroy
-      render json: post
-    else
-      render json: {message: 'can not delete data'}, status: 422
-    end
-  end
+			post = Post.find(params[:id])
+			if current_api_v1_user.id == post.user_id
+					post.destroy
+					render json: post
+			else
+					render json: {message: 'can not delete data'}, status: 422
+			end
+	end
 
 	private
-  def post_params
-    params.require(:post).permit(:content).merge(user_id: current_api_v1_user.id) # 追加
-  end
+	def post_params
+			params.require(:post).permit(:content).merge(user_id: current_api_v1_user.id)
+	end
 end
