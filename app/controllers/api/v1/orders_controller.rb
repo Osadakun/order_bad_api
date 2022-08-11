1a08baff7cb8f4a73de1bf1a83aafa6b719e6589
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-11 17:22:12 +0900

    APIを追加

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,21 +1,13 @@
 class Api::V1::OrdersController < ApplicationController
-  before_action :authenticate_api_v1_user!, only: [:create, :update, :destroy]
+  before_action :authenticate_api_v1_user!, only: []
 	def index
-			posts = Post.all.order(created_at: :desc)
-			posts_array = posts.map do |post|
-				{
-					id: post.id,
-					content: post.content,
-					user: User.find_by(id: post.user_id)
-				}
-			end
-			render json: posts_array
 	end
 
 	def show
     order = Order.find_by(team_id: params[:team_id])
     order_list = {
 			id: order.id,
+      name: order.name,
 			enemy_name: order.enemy_name,
 			first_double_1: order.first_double_1,
 			first_double_2: order.first_double_2,
@@ -27,40 +19,44 @@ class Api::V1::OrdersController < ApplicationController
 		render json: order_list
 	end
 
-	def create
-		post = Post.new(post_params)
-		if post.save
-			render json: post
-		else
-			render json: post.errors, status: 422
-		end
-	end
+  def have_team_all
+    teams = Team.all.where(user_id: params[:team_id]).order(:name)
+    team_array = teams.map do |team|
+      {
+        id: team.id,
+        name: team.name,
+      }
+    end
+    render json: team_array
+  end
 
-	def update
-		post = Post.find(params[:id])
-		if current_api_v1_user.id == post.user_id
-			if post.update(post_params)
-				render json: post
-			else
-				render json: post.errors, status: 422
-			end
-		else
-			render json: {message: 'can not update data'}, status: 422
-		end
-	end
-
-	def destroy
-		post = Post.find(params[:id])
-		if current_api_v1_user.id == post.user_id
-			post.destroy
-			render json: post
-		else
-			render json: {message: 'can not delete data'}, status: 422
-		end
-	end
+  def create
+    members = Team.find(params[:id])
+    member_list = {
+      user_name: members.user_name,
+      name: members.name,
+      member1: [
+        members.member1,
+        members.member2,
+        members.member3,
+        members.member4,
+        members.member5,
+        members.member6,
+        members.member7,
+      ],
+      # member1: members.member1,
+      # member2: members.member2,
+      # member3: members.member3,
+      # member4: members.member4,
+      # member5: members.member5,
+      # member6: members.member6,
+      # member7: members.member7,
+    }
+    render json: member_list
+  end
 
 	private
 	def post_params
-		params.require(:post).permit(:content).merge(user_id: current_api_v1_user.id)
+		params.require(:order).permit(:id, :name, :enemy_name, :first_double_1, :first_double_2, :first_single, :second_double_1, :second_double_2, :team_id)
 	end
 end
