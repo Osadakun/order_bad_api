322b06a3cf74b546dba07c745ffaaac5ada0f654
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-11-24 17:36:59 +0900

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # devise_for :admin_users, ActiveAdmin::Devise.config
-  # ActiveAdmin.routes(self)
   namespace :api do
     namespace :v1 do
       resources :orders, only: %i[index create show], param: :team_id do
